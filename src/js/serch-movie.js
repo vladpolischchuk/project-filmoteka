5fdb955fd718813332c97b327550620ee11509a6
Владислав Поліщук <105670228+vladpolischchuk@users.noreply.github.com>
2023-04-26 08:43:10 +0300

    Update serch-movie.js

--- src/js/serch-movie.js
+++ src/js/serch-movie.js
@@ -1,21 +1,21 @@
-import { fetchGenresMovieAPI, fetchMovieSearchAPI } from './API/fetch-film-api';
-import { createMovieListFMarkup } from './films-render';
-import { refs } from './refs';
+// import { fetchGenresMovieAPI, fetchMovieSearchAPI } from './API/fetch-film-api';
+// import { createMovieListFMarkup } from './films-render';
+// import { refs } from './refs';
 
-const searchForm = document.querySelector('.form-search');
+// const searchForm = document.querySelector('.form-search');
 
-async function onInput(e) {
-    e.preventDefault();
+// async function onInput(e) {
+//     e.preventDefault();
   
-    value = e.target.elements.searchQuery.value.trim();
+//     value = e.target.elements.searchQuery.value.trim();
 
-    await fetchGenresMovieAPI().then(genres => {
-        fetchMovieSearchAPI(value).then(data => {
-            let markup = createMovieListFMarkup(data, genres);
-            refs.popularMovieList.innerHTML = '';
-            refs.popularMovieList.insertAdjacentHTML('beforeend', markup);
-        });
-    });
-  }
+//     await fetchGenresMovieAPI().then(genres => {
+//         fetchMovieSearchAPI(value).then(data => {
+//             let markup = createMovieListFMarkup(data, genres);
+//             refs.popularMovieList.innerHTML = '';
+//             refs.popularMovieList.insertAdjacentHTML('beforeend', markup);
+//         });
+//     });
+//   }
 
-searchForm.addEventListener('submit', onInput);
+// searchForm.addEventListener('submit', onInput);
